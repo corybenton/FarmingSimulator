592ec2209182d648e6fdb7eec8b677b97529144f
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-09-11 20:16:59 -0400

    Started work on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,16 @@
 const { User, Crop } = require('../models');
 
 const resolvers = {
+    Query: {
+        user: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return User.find(params);
+        },
+        
+    },
+    Mutation: {
 
-}
+    },
+};
 
 module.exports = resolvers;
